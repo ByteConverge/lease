ee1aa537a39fe2e1a4d1a796d2384290902a06fd
Oyama <godstimeoyama74@gmail.com>
2025-09-22 00:16:35 +0100

    made changes for the case sensistivity

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 
 // Get all users (admin only)
 const getUsers = async (req, res) => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require('../models/User');
-const Land = require('../models/Land');
+const Land = require('../models/land');
 const Equipment = require('../models/Equipment');
 const { auth, authorizeAdmin } = require('../middleware/auth');
 
